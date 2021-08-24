31ad6614ae89984146bf7127cc4297d643bb5f5d
Mariia-Kosorotykova <mariia.kosorotykova@gmail.com>
2021-08-24 20:44:11 +0300

    Fixed Migration

--- services/web/project/config.py
+++ services/web/project/config.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "postgresql://postgres:postgres_password@localhost:5432/postgres_db")
+        "postgresql://postgres:postgres_password@db_films/postgres_db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
